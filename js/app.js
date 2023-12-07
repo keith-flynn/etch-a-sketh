f433bf980103f7468743cde286b714b2c7ba0fa1
Keith Flynn <flynn.keith@gmail.com>
2023-12-07 14:19:36 -0500

    Add comments and minor styling

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 'use-strict';
 
+// Generate panels (each representing a pixel)
 const container = document.getElementById('container');
 
-for (let i = 0; i < 4000; i++) {
+for (let i = 0; i < 3000; i++) {
   const panel = document.createElement('div');
   panel.classList.add('panel');
   container.appendChild(panel);
 }
 
+// Double event listeners for mousedown and mouseenter
+// A click-and-hold to draw gesture
 const panels = document.querySelectorAll('.panel');
 
 let isMouseDown = false;
@@ -20,6 +23,7 @@ document.addEventListener('mouseup', () => {
   isMouseDown = false;
 });
 
+// Add event listener for each panel
 panels.forEach((panel) => {
   panel.addEventListener("mouseenter", (e) => {
     if (isMouseDown) {
@@ -27,3 +31,10 @@ panels.forEach((panel) => {
     }
   });
 });
+
+// Reset button event listener
+const resetButton = document.getElementById("reset")
+
+resetButton.addEventListener("click", (e) => {
+  location.reload();
+})
