fab383fb0e18694f15735073244d625ddadfad2f
Keith Flynn <flynn.keith@gmail.com>
2023-12-10 16:05:04 -0500

    Add controls for grid size
    Add HTML inputs and JavaScript code for changing the size of the
    container. Also change styles to reflect input changes. Still needs
    cleaned up and some guard rails to not generate too many divs.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,40 @@
 'use-strict';
-
-// Generate panels (each representing a pixel)
+const gridSizeInput = document.getElementById("grid-size-input")
+const submitButton = document.getElementById("submit");
+const resetButton = document.getElementById("reset")
 const container = document.getElementById('container');
 
-for (let i = 0; i < 16; i++) {
-  const panelRow = document.createElement('div');
-  //panelRow.classList.add('panel');
-  panelRow.classList.add('row');
-  container.appendChild(panelRow);
-  for (let j = 0; j < 16; j++) {
-    const panelColumn = document.createElement('div');
-    panelColumn.classList.add('panel');
-    panelColumn.classList.add('column');
-    panelRow.appendChild(panelColumn);
+let gridSizeValue = 10;
+
+// Generate panels (each representing a pixel)
+function generatePanels() {
+  container.replaceChildren();
+  for (let i = 0; i < gridSizeValue; i++) {
+    const panelRow = document.createElement('div');
+    //panelRow.classList.add('panel');
+    panelRow.classList.add('row');
+    container.appendChild(panelRow);
+    for (let j = 0; j < gridSizeValue; j++) {
+      const panelColumn = document.createElement('div');
+      panelColumn.classList.add('panel');
+      panelColumn.classList.add('column');
+      panelRow.appendChild(panelColumn);
+    }
   }
+  //addPanelListeners();
+}
+
+generatePanels();
+
+function changePanels() {
+  gridSizeValue = gridSizeInput.value;
+  generatePanels();
+  addPanelListeners();
 }
 
 // Double event listeners for mousedown and mouseenter
 // A click-and-hold to draw gesture
-const panels = document.querySelectorAll('.panel');
+
 
 let isMouseDown = false;
 
@@ -31,17 +47,22 @@ document.addEventListener('mouseup', () => {
 });
 
 // Add event listener for each panel
-panels.forEach((panel) => {
-  panel.addEventListener("mouseover", (e) => {
-    if (isMouseDown) {
-      e.target.style.backgroundColor = "green";
-    }
+function addPanelListeners() {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((panel) => {
+    panel.addEventListener("mouseover", (e) => {
+      if (isMouseDown) {
+        e.target.style.backgroundColor = "green";
+      }
+    });
   });
-});
+}
+addPanelListeners();
 
 // Reset button event listener
-const resetButton = document.getElementById("reset")
-
 resetButton.addEventListener("click", (e) => {
   location.reload();
 })
+
+// Change grid size event listener
+submitButton.addEventListener("click", changePanels);
